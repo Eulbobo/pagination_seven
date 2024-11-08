867c4a0925b621d70e7b85cb64574582368a07ad
Julien Boniol <julien.boniol@adod.fr>
2024-11-08 14:14:17 +0100

    encore l'implementation la plus simple

--- src/main/java/fr/eulbobo/dojo/pagination/PartialPageDisplay.java
+++ src/main/java/fr/eulbobo/dojo/pagination/PartialPageDisplay.java
@@ -6,8 +6,8 @@ public class PartialPageDisplay implements PageDisplay {
 
     @Override
     public void displayWith(Displayer displayer, int totalPages, int selectedPage) {
-        if (selectedPage - 1 == 1) {
-            displayer.print(ValueDisplay.displayValues(selectedPage - 1, selectedPage + 3, selectedPage) + PARTIAL_DISPLAY_SEPARATOR + totalPages);
+        if (selectedPage < 5) {
+            displayer.print(ValueDisplay.displayValues(1, 5, selectedPage) + PARTIAL_DISPLAY_SEPARATOR + totalPages);
         } else {
             displayer.print("1" + PARTIAL_DISPLAY_SEPARATOR + ValueDisplay.displayValues(selectedPage - 1, selectedPage + 1, selectedPage) + PARTIAL_DISPLAY_SEPARATOR + totalPages);
         }
