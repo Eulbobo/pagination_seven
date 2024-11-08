a8d983ee645b3a92d52698ab025edbcb6a62cb22
Julien Boniol <julien.boniol@adod.fr>
2024-11-08 12:10:11 +0100

    parameterized test

--- src/test/java/fr/eulbobo/dojo/pagination/PaginationTest.java
+++ src/test/java/fr/eulbobo/dojo/pagination/PaginationTest.java
@@ -1,62 +1,39 @@
 package fr.eulbobo.dojo.pagination;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class PaginationTest {
 
-    @Test
-    void should_highlight_1_when_created_with_5_pages() {
-        Displayer displayer = mock(Displayer.class);
-        Pagination pagination = Pagination.withPages(5);
-
-        pagination.displayTo(displayer);
-        verify(displayer).print("(1) 2 3 4 5");
+    public static Stream<Arguments> paginationValues() {
+        return Stream.of(
+                Arguments.of(5, 1, "(1) 2 3 4 5"),
+                Arguments.of(5, 2, "1 (2) 3 4 5"),
+                Arguments.of(7, 6, "1 2 3 4 5 (6) 7"),
+                Arguments.of(100, 42, "1 … 41 (42) 43 … 100"),
+                Arguments.of(9, 5, "1 … 4 (5) 6 … 9"),
+                Arguments.of(9, 2, "1 (2) 3 4 5 … 9"),
+                Arguments.of(9, 4, "1 2 3 (4) 5 … 9")
+        );
     }
 
-    @Test
-    void should_highlight_2_when_created_with_5_pages_and_page_2_is_selected() {
+    @ParameterizedTest
+    @MethodSource("paginationValues")
+    void should_display_result_given_circumstances(int pages, int selectedPage, String expectedResult) {
         Displayer displayer = mock(Displayer.class);
-        Pagination pagination = Pagination.withPages(5);
+        Pagination pagination = Pagination.withPages(pages);
 
-        pagination.selectPage(2);
+        if (selectedPage > 1) {
+            pagination.selectPage(selectedPage);
+        }
 
         pagination.displayTo(displayer);
-        verify(displayer).print("1 (2) 3 4 5");
-    }
-
-    @Test
-    void should_highlight_6_when_created_with_7_pages_and_page_6_is_selected() {
-        Displayer displayer = mock(Displayer.class);
-        Pagination pagination = Pagination.withPages(7);
-
-        pagination.selectPage(6);
-
-        pagination.displayTo(displayer);
-        verify(displayer).print("1 2 3 4 5 (6) 7");
-    }
-
-    @Test
-    void should_highlight_page_in_middle_of_list_if_list_has_a_lot_of_pages() {
-        Displayer displayer = mock(Displayer.class);
-        Pagination pagination = Pagination.withPages(100);
-
-        pagination.selectPage(42);
-
-        pagination.displayTo(displayer);
-        verify(displayer).print("1 … 41 (42) 43 … 100");
-    }
-
-    @Test
-    void should_highlight_page_in_middle_of_list_with_9_pages() {
-        Displayer displayer = mock(Displayer.class);
-        Pagination pagination = Pagination.withPages(9);
-
-        pagination.selectPage(5);
-
-        pagination.displayTo(displayer);
-        verify(displayer).print("1 … 4 (5) 6 … 9");
+        verify(displayer).print(expectedResult);
     }
 }
